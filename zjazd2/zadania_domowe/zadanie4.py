233e46a5795b65fa4cda30d1b21302f7c306737a
Robercik <robert.glodek75@gmail.com>
2018-09-06 18:41:24 +0200

    zadania domowe1

--- zjazd2/zadania_domowe/zadanie4.py
+++ zjazd2/zadania_domowe/zadanie4.py
@@ -5,18 +5,18 @@ Podpowiedź: do podzielenia napisu na słowa użyj metody split()
 
 
 def policz_slowa(tekst):
-    #temp = tekst.split()
+    temp = tekst.lower()
+    temp = temp.split()
     slowa = {}
 
-    for i in tekst.split():
+    for i in temp:
         if i in slowa:
-            slowa.get(i)
-            slowa[i] = 1
-        else:
-            slowa[i] += 1
+            slowa[i] +=1
 
+        else:
+            slowa[i] =1
 
     return slowa
 
 
-print(policz_slowa('ala ma kota i kota ma ala'))
+print(policz_slowa('Ala ma kota i kota ma ala'))
