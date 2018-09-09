fb991dd805085f8f982f99d150bd4b0cf432c9e1
Robert Głodek <robert.glodek75@gmail.com>
2018-09-09 17:00:53 +0200

    zjazd3_1

--- zjazd3/OPP/zadanie7.py
+++ zjazd3/OPP/zadanie7.py
@@ -3,7 +3,9 @@ from zjazd3.OPP.zadanie2 import Employee
 
 class PremiumEmployee(Employee):
 
-    def __init__(self, name='Jan', lastname='Kowalski', wage=100, bonus = 0):
+
+
+    def __init__(self, name='Jan', lastname='Kowalski', wage=50, bonus = 0):
         super().__init__(name, lastname, wage)
         self.bonus = bonus
 
@@ -17,6 +19,27 @@ class PremiumEmployee(Employee):
         sal += self.bonus
         self.bonus = 0
         return sal
+
+    @classmethod
+    def creat_hearo(cls):
+        return PremiumEmployee('pracownik','miesiąca', 0.0)
+
+    @classmethod
+    def em_from_string(cls, str_params):
+        lista_param = str_params.split(',')
+        return PremiumEmployee(lista_param[0], lista_param[1], int(lista_param[2]))
+
+    @staticmethod
+    def say_hello:
+        return 'Hello'
+
+
+def test_import_from_tekst():
+    param = 'Henryk,Kania, 50'
+    emp = PremiumEmployee.em_from_string(param)
+    assert emp.name == 'Henryk'
+    assert emp.lastname == 'Kania'
+    assert emp.wage == 50
 
 
 def test_creat_premium_employee():
@@ -34,3 +57,9 @@ def test_register():
     emp.give_bonnus(400)
     assert emp.bonus == 1400
     assert emp.pay_salary() == 1900
+'''
+def test_employee_of_the_month()
+    emp = PremiumEmployee.creat_hearo()
+
+    assert emp.pay_salary == 0
+'''
